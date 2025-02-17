9f76bf3e33401d81450878b86e99b1fccff5634e
ben5978lab <ben.rose5978@gmail.com>
2025-02-17 11:06:49 +0100

    fix css recipe page, fix margin footer

--- client/src/router/routes.tsx
+++ client/src/router/routes.tsx
@@ -9,7 +9,6 @@ import LegalNotices from "../pages/Legal-notices/LegalNotices";
 import Login from "../pages/Login/AuthForm";
 import ViewProfile from "../pages/ViewProfile/ViewProfile";
 import Account from "../pages/account/Account";
-import Catalogue from "../pages/catalogue/Catalogue";
 import DashboardAdmin from "../pages/dashboard-admin/DashBoardAdmin";
 import DashboardRecipes from "../pages/dashboard-admin/DashboardRecipes";
 import DashBoardUser from "../pages/dashboard-admin/DashboardUser";
@@ -23,10 +22,6 @@ const routes = [
   {
     path: "/account",
     element: <Account />,
-  },
-  {
-    path: "/catalogue",
-    element: <Catalogue />,
   },
   {
     path: "/create-recipe",
