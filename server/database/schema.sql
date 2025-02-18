7ba6aff5ae7c7c2e4fe63773f10d3ed27b2a0a52
Largoet <prunet.t@gmail.com>
2025-02-18 10:33:20 +0100

    correction bdd NOT NULL pour la date d'inscription

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -4,7 +4,7 @@ CREATE TABLE utilisateur (
     pseudo VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL UNIQUE,
     mot_de_passe VARCHAR(255) NOT NULL,
-    date_inscription DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    date_inscription DATETIME DEFAULT CURRENT_TIMESTAMP,
     photo_profil VARCHAR(255),
     est_admin BOOLEAN NOT NULL DEFAULT FALSE
 );
